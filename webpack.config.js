a6978aee0c02b46bebadc52ca1e8b4781651522e
Boris <bfeen@mail.ru>
2020-06-09 23:04:42 +0300

    1.2.2 Installed and setting up Webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+const publicPath = path.join(__dirname, `public`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: publicPath
+  },
+  devServer: {
+    contentBase: publicPath,
+    open: true,
+    port: 1337
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
